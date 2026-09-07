4dc6ce8a7be3840e7aee1aa3b639913b497153ab
Amy Trompeter <amy.trompeter@slalom.com>
2026-09-07 10:01:35 -0500

    chore: add Vuetify 3, plugin, vite-plugin-vuetify, and mdi icons

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
 // Ensure dark mode by default (can be toggled by component)
 document.documentElement.classList.add('dark')
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(vuetify).use(router).mount('#app')
